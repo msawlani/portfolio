cb1bfc526e7379a9abdf110615aaf0c38c8bffa5
Michael <39811434+msawlani@users.noreply.github.com>
2020-07-20 10:55:18 -0500

    starts in routes and imported a bunch of needed things

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Routes from "./routes";
+import * as serviceWorker from "./serviceWorker";
+import Home from "./Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Routes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
