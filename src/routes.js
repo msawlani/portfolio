3bfe6cc138ee396c47f17316e6f129a647c575ae
Michael <39811434+msawlani@users.noreply.github.com>
2020-12-24 17:11:44 -0600

    added skills route and copyright
    copyright here will show on all pages instead of adding to each page

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./Home";
 import Nav from "./Navbar";
 import Error404 from "./404error/404";
-import Projects from "./projects/projects";
+import Projects from "./projects";
+import About from "./skills";
 import "./routes.css";
+const Copyright = React.lazy(() => import("./shared/copyright"));
+
 
 const Routes = () => {
   return (
@@ -17,9 +20,11 @@ const Routes = () => {
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/projects" exact component={Projects} />
+              <Route path="/skills" exact component={About}/>
               <Route path="**" exact component={Error404} />
             </Switch>
           </Fragment>
+          <Copyright/>
         </Fragment>
       </Suspense>
     </Router>
