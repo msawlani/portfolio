dd89491577ef43b95541c03830d9c2df8367590d
Michael <39811434+msawlani@users.noreply.github.com>
2020-12-24 17:12:21 -0600

    made a skills js file to work on later
    will include personal and technical skills

---
+++ src/skills.js
@@ -0,0 +1,13 @@
+import React, { useState, Fragment } from "react";
+
+const Skills = () =>{
+    return (
+      <Fragment>
+        <h1 className="mt-5 text-center">Skills</h1>
+        <h2 className="pl-3">Technical</h2>
+        <h2 className="pl-3">Personal</h2>
+      </Fragment>
+    );
+}
+
+export default Skills;
