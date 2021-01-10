70a6bb425834d3d484c778a323e5ae0666d81310
Michael <39811434+msawlani@users.noreply.github.com>
2021-01-09 21:23:51 -0600

    added a list of skills that i have

--- src/skills.js
+++ src/skills.js
@@ -5,7 +5,16 @@ const Skills = () =>{
       <Fragment>
         <h1 className="mt-5 text-center">Skills</h1>
         <h2 className="pl-3">Technical</h2>
+        <ul>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>C#</li>
+          <li>Java</li>
+          <li>Swift</li>
+        </ul>
         <h2 className="pl-3">Personal</h2>
+        <h2 className="pl-3">Platforms</h2>
       </Fragment>
     );
 }
