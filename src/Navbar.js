2778133b9d62e0dd298f80b94b86c4c8763e933e
Michael <39811434+msawlani@users.noreply.github.com>
2021-11-09 16:44:39 -0600

    Dark and light, cleaned up css, added lots of bootstrap, and now have drop down menu for mobile

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,38 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import pdf from "./assets/Michael_Sawlani_Resume.pdf";
 import "./CSS/nav.css";
-import Links from "./data/Links.json"
-const nav = () =>{
-  return (
-    <nav className="d-flex">
-      <div class="d-flex">
-        <h3 className="name pl-4">Michael Sawlani</h3>
-        <a
-          className="pl-4 pt-1"
-          style={{ fontSize: "20px" }}
-          href={pdf}
-          target="_blank"
-          download={pdf}
-        >
-          Resume
-        </a>
-      </div>
+import Links from "./data/Links.json";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import { lightTheme, darkTheme } from "./Theme";
 
-      <div className="d-flex">
-        {Links.Links.map((link) => (
-          <NavLink
-            className="main-nav p-4"
-            activeClassName="main-nav-active"
-            exact
-            to={link.To}
-          >
-            {link.Name}
-          </NavLink>
-        ))}
-      </div>
-    </nav>
+const Nav = () => {
+  const [theme, setTheme] = useState("light");
+  const themeToggle = () => {
+    theme == "light" ? setTheme("dark") : setTheme("light");
+  };
+  return (
+    <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <nav class="navbar navbar-light navbar-expand-sm rounded">
+        <h3>Michael Sawlani</h3>
+        <button onClick={themeToggle}>{theme}</button>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbars"
+          aria-controls="navbars"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbars" class="collapse navbar-collapse ">
+          <ul class="list-unstyled navbar-nav ml-auto mr-lg-2 ">
+            {Links.Links.map((link) => (
+              <li>
+                <NavLink
+                  className="main-nav pr-3"
+                  activeClassName="main-nav-active"
+                  exact
+                  to={link.To}
+                >
+                  {link.Name}
+                </NavLink>
+              </li>
+            ))}
+            <li>
+              <a href={pdf} target="_blank" download={pdf}>
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </ThemeProvider>
   );
 };
 
-export default nav;
+export default Nav;
