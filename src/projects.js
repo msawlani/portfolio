df9ca8516a5e93c8684453ff674cb4a74013746d
Michael <39811434+msawlani@users.noreply.github.com>
2021-11-22 16:46:33 -0600

    made projects look better and added json data to it

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,7 @@
 import React, { Fragment } from "react";
 import "./CSS/projects.css";
-import projects from "./data/Projects.json"
-import projects2 from "./data/Projects2.json";
- 
+import projects from "./data/Projects.json";
+
 const Projects = () => {
   return (
     <Fragment>
@@ -11,67 +10,29 @@ const Projects = () => {
       </Fragment>
       <Fragment>
         <ul>
-          {projects.MobileProjects.map((projects) => (
+          {projects.Projects.map((projects) => (
             <div className="pr-4 pb-4">
               <div className="card d-flex m-4">
                 <div className="card-title mt-2 ml-4"></div>
                 <div className="card-body">
-                  <div className="images p-4">
-                    {projects.Images.map((image) => (
+                  {projects.Images.map((image) => (
+                    <div className="col-sm-6">
                       <img
-                        class="mobileImg"
+                        class="img-fluid rounded  w-100 h-100"
                         src={require(`./assets/Images/${image}.png`)}
                       />
-                    ))}
-                  </div>
-                  <h2 className="">
-                    {projects.Name} | {projects.Platform}
-                  </h2>
-                  <div className="">
-                    <h2>Technical</h2>
-                    <ul>
-                      {projects.Technical.map((technical) => (
-                        <li>{technical}</li>
-                      ))}
-                    </ul>
-                    <h2>Tasks</h2>
-                    <ul>
-                      {projects.Tasks.map((tasks) => (
-                        <li className="task">{tasks}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-          {projects2.DesktopProjects.map((projects) => (
-            <div className="pr-4 pb-4">
-              <div className="card d-flex m-4">
-                <div className="card-title mt-2 ml-4"></div>
-                <div className="images p-4">
-                  {projects.Images.map((image) => (
-                    <img
-                      class="desktopImg"
-                      src={require(`./assets/Images/${image}.png`)}
-                    />
+                    </div>
                   ))}
-                </div>
-                <div className="card-body">
+
                   <h2 className="">
                     {projects.Name} | {projects.Platform}
                   </h2>
+                  <p>{projects.Description}</p>
                   <div className="">
                     <h2>Technical</h2>
                     <ul>
                       {projects.Technical.map((technical) => (
                         <li>{technical}</li>
-                      ))}
-                    </ul>
-                    <h2>Tasks</h2>
-                    <ul>
-                      {projects.Tasks.map((tasks) => (
-                        <li className="task">{tasks}</li>
                       ))}
                     </ul>
                   </div>
