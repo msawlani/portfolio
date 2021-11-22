8d417398b819d3715926e6001342808c52190746
Michael <39811434+msawlani@users.noreply.github.com>
2021-11-22 16:47:13 -0600

    imported bootstrap

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "./CSS/index.css";
 import Routes from "./routes";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
-
