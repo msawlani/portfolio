ce7caf8bdcf50b954e0790f5e79fccf003476625
Michael <sawlani.michael@yahoo.com>
2021-11-29 15:17:09 -0600

    made design improvements and added a contact form

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,50 @@
-import React, { useState, Fragment } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMixcloud, faTwitch } from "@fortawesome/free-brands-svg-icons";
+import React from "react";
 import "./CSS/home.css";
 import skills from "./data/Skills.json";
 import picture from "./assets/Images/picture.jpg";
 
 const App = () => {
   return (
-    <div className="container body">
-      <Fragment className="wrapper">
-        <Fragment>
-          <div className="col-sm-2">
-            <img className="rounded-circle img-fluid" src={picture} />
-          </div>
-        </Fragment>
-        <h2 className="ml-auto">About Me</h2>
-        <ul className="ml-auto">
-          {skills.Skills.map((skill) => (
-            <Fragment>
-              <h4 className="pt-4">{skill.Name}</h4>
-              {skill.Skills.map((name) => (
-                <li>{name}</li>
-              ))}
-            </Fragment>
+    <div className="container">
+      <div className="d-flex text-white">
+      <div className="col-lg-3">
+          <img className="rounded-circle img-fluid" src={picture} />
+         <h2>About Me</h2>
+          <p>I am a Software Developer determind on building products that change the world. </p>
+      </div>
+      <div className="col-lg-5 offset-lg-5">
+ <form>
+ <h2>Contact Me</h2>
+   <div className="mb-3">
+     <label for="emailInput" class="form-label">Email Address</label>
+     <input type="email" id="emailInput" class="form-control"/>
+   </div>
+   <div className="mb-3">
+     <label for="subjectInput" class="form-label">Subject</label>
+     <input type="text" id="subjectInput" class="form-control"/>  
+     </div>
+   <div className="mb-3">
+     <label for="messageTextArea" class="form-label">Message</label>
+     <textarea class="form-control" id="messaageTextArea" rows="3"></textarea>
+   </div>
+ </form>
+ </div>
+      </div>
+      <div className="d-flex text-white">
+        <div className="col-lg-2">            
+        <h4>{skills.Lang.Name}</h4>
+          {skills.Lang.Skills.map((name) => (
+            <li>{name}</li>
           ))}
-        </ul>
-      </Fragment>
+        </div>
+        <div>
+          <h4>{skills.Plat.Name}</h4>
+          {skills.Plat.Skills.map((name) => (
+            <li>{name}</li>
+
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
