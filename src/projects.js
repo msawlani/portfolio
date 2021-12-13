7cdc54b37b930b91dd1d6add095d1f15bac7e7a1
Michael <39811434+msawlani@users.noreply.github.com>
2021-12-13 13:12:49 -0600

    carousel
    added a carousel with indicators, buttons, and it all comes from the json file.

--- src/projects.js
+++ src/projects.js
@@ -1,51 +1,82 @@
+import { Button } from "bootstrap";
 import React, { Fragment } from "react";
 import "./CSS/projects.css";
 import projects from "./data/Projects.json";
-import { Carousel } from 'react-responsive-carousel';
-
 
 const Projects = () => {
   return (
-    <Fragment>
+    <div className=" text-white ">
       <Fragment>
-        <h1 className="mt-5 text-white text-center">Projects</h1>
+        <h1 className="pb-5 text-center">Projects</h1>
       </Fragment>
-      <Fragment>
-      <div className="carousel-wrapper">
 
-<Carousel>
-  {projects.Projects.map((projects) => (
+      <div
+        id="carouselProjects"
+        class="carousel slide position-absolute"
+        data-bs-ride="carousel"
+      >
+        <ol class="carousel-indicators position-fixed">
+          {projects.Projects.map((projects, index) => (
+            <li
+              data-target="#carouselProjects"
+              data-slide-to={index}
+              class={projects.classI}
+            ></li>
+          ))}
+        </ol>
+        <div className="carousel-inner">
+          {projects.Projects.map((projects) => (
+            <div className={projects.class}>
+              <div className="container">
+                {projects.Images.map((image) => (
+                  <div className="">
+                    <img
+                      class="img-fluid rounded"
+                      src={require(`./assets/Images/${image}.png`)}
+                    />
+                  </div>
+                ))}
+                <div className="">
+                  <h2 className="">
+                    {projects.Name} | {projects.Platform}
+                  </h2>
+                  <a href={projects.Website} rel="noopener" target="_blank">
+                    {projects.Name}
+                  </a>
+                  <p className="">{projects.Description}</p>
 
-        <div className="card">
-          {projects.Images.map((image) => (
-            <div className="card-img-top">
-              <img
-                class="img-fluid rounded"
-                src={require(`./assets/Images/${image}.png`)}
-              />
+                  <h2>Technical</h2>
+                  <ul>
+                    {projects.Technical.map((technical) => (
+                      <li>{technical}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           ))}
-<div className="card-body">
-          <h2 className="card-title">
-            {projects.Name} | {projects.Platform}
-          </h2>
-          <p className="card-text">{projects.Description}</p>
-          
-            <h2>Technical</h2>
-            <ul>
-              {projects.Technical.map((technical) => (
-                <li>{technical}</li>
-              ))}
-            </ul>
-          </div>
         </div>
+        <a
+          class="carousel-control-prev position-fixed"
+          href="#carouselProjects"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
 
-    
-  ))}
-               </Carousel>
-  </div>
-      </Fragment>
-    </Fragment>
+        <a
+          class="carousel-control-next position-fixed"
+          href="#carouselProjects"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
   );
 };
 
