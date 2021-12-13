803a400d0e0253efb4ad44711cee3db8c2a40bd6
Michael <39811434+msawlani@users.noreply.github.com>
2021-12-13 13:10:53 -0600

    contact form and fix bio
    still working on form positioning and mobile stuff and split skills

--- src/Home.js
+++ src/Home.js
@@ -7,32 +7,45 @@ const App = () => {
   return (
     <div className="container">
       <div className="d-flex text-white">
-      <div className="col-lg-3">
+        <div className="col-lg-3">
           <img className="rounded-circle img-fluid" src={picture} />
-         <h2>About Me</h2>
-          <p>I am a Software Developer determind on building products that change the world. </p>
-      </div>
-      <div className="col-lg-5 offset-lg-5">
- <form>
- <h2>Contact Me</h2>
-   <div className="mb-3">
-     <label for="emailInput" class="form-label">Email Address</label>
-     <input type="email" id="emailInput" class="form-control"/>
-   </div>
-   <div className="mb-3">
-     <label for="subjectInput" class="form-label">Subject</label>
-     <input type="text" id="subjectInput" class="form-control"/>  
-     </div>
-   <div className="mb-3">
-     <label for="messageTextArea" class="form-label">Message</label>
-     <textarea class="form-control" id="messaageTextArea" rows="3"></textarea>
-   </div>
- </form>
- </div>
+          <h2>About Me</h2>
+          <p>
+            I am a Software Developer determined on building products that
+            change the world.
+          </p>
+        </div>
+        <div className="col-lg-5 offset-lg-4">
+          <form>
+            <h2>Contact Me</h2>
+            <div className="mb-3">
+              <label for="emailInput" class="form-label">
+                Email Address
+              </label>
+              <input type="email" id="emailInput" class="form-control" />
+            </div>
+            <div className="mb-3">
+              <label for="subjectInput" class="form-label">
+                Subject
+              </label>
+              <input type="text" id="subjectInput" class="form-control" />
+            </div>
+            <div className="mb-3">
+              <label for="messageTextArea" class="form-label">
+                Message
+              </label>
+              <textarea
+                class="form-control"
+                id="messaageTextArea"
+                rows="3"
+              ></textarea>
+            </div>
+          </form>
+        </div>
       </div>
       <div className="d-flex text-white">
-        <div className="col-lg-2">            
-        <h4>{skills.Lang.Name}</h4>
+        <div className="col-lg-2">
+          <h4>{skills.Lang.Name}</h4>
           {skills.Lang.Skills.map((name) => (
             <li>{name}</li>
           ))}
@@ -41,7 +54,6 @@ const App = () => {
           <h4>{skills.Plat.Name}</h4>
           {skills.Plat.Skills.map((name) => (
             <li>{name}</li>
-
           ))}
         </div>
       </div>
