339af726efd12602d68357043f2e18df8f52aa9a
Michael <39811434+msawlani@users.noreply.github.com>
2022-01-23 16:50:02 -0600

    emailjs
    implemented emailjs onto the submit part of the form. made text fields required and linked them to the form api

--- src/contact.js
+++ src/contact.js
@@ -1,34 +1,78 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 import "./CSS/home.css";
 
 const Content = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "yahoo",
+        "template_qxl3m6s",
+        form.current,
+        "user_AlZxpeQYXOwdCLwIRk2ey"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
+  };
   return (
     <div className="container">
       <div className="d-flex text-white row">
         <div className="col-lg-5 offset-lg-4">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <h2>Contact Me</h2>
             <div className="mb-3">
-              <label for="emailInput" class="form-label">
+              <label htmlFor="emailInput" className="form-label">
                 Email Address
               </label>
-              <input type="email" id="emailInput" class="form-control" />
+              <input
+                type="email"
+                id="emailInput"
+                className="form-control"
+                name="email"
+                required
+              />
             </div>
             <div className="mb-3">
-              <label for="subjectInput" class="form-label">
+              <label htmlFor="subjectInput" className="form-label">
                 Subject
               </label>
-              <input type="text" id="subjectInput" class="form-control" />
+              <input
+                type="text"
+                id="subjectInput"
+                className="form-control"
+                name="subject"
+                required
+              />
             </div>
             <div className="mb-3">
-              <label for="messageTextArea" class="form-label">
+              <label htmlFor="messageTextArea" className="form-label">
                 Message
               </label>
               <textarea
-                class="form-control"
+                className="form-control"
                 id="messaageTextArea"
                 rows="3"
+                name="message"
+                required
               ></textarea>
+            </div>
+            <div className="mb-3 text-center">
+              <input
+                type="submit"
+                className="btn btn-info"
+                value="Send Message"
+              />
             </div>
           </form>
         </div>
