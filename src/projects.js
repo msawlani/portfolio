05c8e4710e3dbd945cb1dc5f8d579664be99ff1e
Michael <39811434+msawlani@users.noreply.github.com>
2022-01-23 16:59:51 -0600

    fixed projects
    removed carousel and went back to old way of making different divs/cards with 2 per row on desktop

--- src/projects.js
+++ src/projects.js
@@ -1,85 +1,47 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./CSS/projects.css";
+import { Link } from "react-router-dom";
 import projects from "./data/Projects.json";
 
 const Projects = () => {
   return (
-    <div className=" text-white ">
-      <Fragment>
-        <h1 className="pb-5 text-center">Projects</h1>
-      </Fragment>
+    <div className="text-white">
+      <h1 className="pb-5 text-center">Projects</h1>
 
-      <div
-        id="carouselProjects"
-        class="carousel slide position-aboslute"
-        data-bs-ride="carousel"
-      >
-          <div class="carousel-indicators position-fixed">
-          {projects.Projects.map((projects, index) => (
-            <button
-              type="button"
-              data-target="#carouselProjects"
-              data-bs-slide-to={index}
-              class={projects.classI}
-              aria-current={projects.aCurrent}
-              aria-label={projects.Name}
-            ></button>
-          ))}
-  </div>
+      <div className="container">
+        <div className="row row-cols-2 g-3">
+          {projects.Projects.map((projects, id) => (
+            <div key={id}>
+              <div className="position-relative">
+                <img
+                  className="rounded img-fluid"
+                  src={require(`./assets/Images/${projects.Image}.png`)}
+                  alt={projects.Image}
+                />
+                <p className="fs-1 fw-bold">
+                  {projects.Name} | {projects.Platform}
+                </p>
+                <a
+                  className="fs-5"
+                  href={projects.Website}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {projects.Website === "" ? "" : projects.Name}
+                </a>
 
-        <div className="carousel-inner">
-          {projects.Projects.map((projects) => (
-            <div className={projects.class}>
-              <div className="container d-flex justify-content-center">
-                {projects.Images.map((image) => (
-                  <div className="">
-                    <img
-                      class="img-fluid rounded"
-                      src={require(`./assets/Images/${image}.png`)}
-                    />
-                  </div>
-                ))}
-                <div className="">
-                  <h2 className="">
-                    {projects.Name} | {projects.Platform}
-                  </h2>
-                  <a href={projects.Website} rel="noopener" target="_blank">
-                    {projects.Name}
-                  </a>
-                  <p className="">{projects.Description}</p>
+                <p className="">{projects.Description}</p>
 
-                  <h2>Technical</h2>
-                  <ul>
-                    {projects.Technical.map((technical) => (
-                      <li>{technical}</li>
-                    ))}
-                  </ul>
-                </div>
+                <p className="fs-4 fw-bold">Technical</p>
+                <ul>
+                  {projects.Technical.map((technical, id) => (
+                    <li key={id}>{technical}</li>
+                  ))}
+                </ul>
               </div>
             </div>
           ))}
         </div>
-        <button
-          class="carousel-control-prev position-fixed"
-          data-bs-target="#carouselProjects"
-          type="button"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-
-        </button>
-
-        <button
-          class="carousel-control-next position-fixed"
-          data-bs-target="#carouselProjects"
-          type="button"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-
-        </button>
       </div>
     </div>
   );
