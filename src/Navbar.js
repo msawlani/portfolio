18c700553e1bc748f4a1fa7c77f68fe84e82ad75
Michael <39811434+msawlani@users.noreply.github.com>
2022-01-23 16:54:19 -0600

    finished Navbar
    fix color of links
    fixed nav bar to work properly
    removed unnecessary things

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./CSS/nav.css";
 import Links from "./data/Links.json";
 
 const Nav = () => {
   return (
-    <nav class="navbar navbar-expand-sm navbar-dark">
-      <h3 className="text-white">Michael Sawlani</h3>
-      <button
-        class="navbar-toggler ms-auto"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbars"
-        aria-controls="navbars"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon "></span>
-      </button>
-
-      <div
-        id="navbars"
-        class="collapse navbar-collapse d-flex justify-content-around"
-      >
-        <ul class="navbar-nav">
-          {Links.Links.map((link) => (
-            <li>
-              <NavLink
-                className="main-nav m-3 text-decoration-none"
-                activeClassName="main-nav-active"
-                exact
-                to={link.To}
-              >
-                {link.Name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
+    <nav className="navbar navbar-expand-sm navbar-dark">
+      <div className="container">
+        <span className="align-middle text-white fs-1 fw-bold col-md-8 m-3">
+          Michael Sawlani
+        </span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar"
+          aria-controls="navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse col-6" id="navbar">
+          <ul className="navbar-nav">
+            {Links.Links.map((link, id) => (
+              <li className="align-middle" key={id}>
+                <NavLink
+                  className="main-nav m-3 text-decoration-none"
+                  activeClassName="main-nav-active"
+                  exact
+                  to={link.To}
+                >
+                  {link.Name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
