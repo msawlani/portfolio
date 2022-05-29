603678399d36765ca4353b5ddbd4ad94a7efbc39
Michael <39811434+msawlani@users.noreply.github.com>
2022-05-29 16:07:14 -0500

    Updated about
    added a bio explaining who michael is
    
    fix a bug with skills styling but using col-12 and col-sm-6

--- src/about.js
+++ src/about.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./CSS/home.css";
 import skills from "./data/Skills.json";
+import education from "./data/Education.json";
 import { NavLink } from "react-router-dom";
 import pdf from "./assets/Michael_Sawlani_Resume.pdf";
 
 const About = () => {
   return (
     <div className="container text-white">
-      <p>BIO</p>
+      <h1>BIO</h1>
+      <p className="p-4">
+        Michael is a Full Stack Developer with 5+ years in the industry and
+        currently works as a Mulesoft Developer for a Sogeti client KPMG. He
+        graduated in 2019 with a Software Development Bachelors from Full Sail
+        University and is now pursing a Masters in Information Technology &
+        Computer Engineering at Atlantis University. He is expected to graduate
+        by the end of 2022 While doing his Bachelors, he was working as a remote
+        Web Developer for MD Pediatric Center. So far the skills that he has
+        picked up are HTML, JavaScript, TypeScript, CSS, React, Angular, etc.
+        His ultimate goal as a developer is to continue to thrive and to have a
+        impact on the world today.
+      </p>
       <NavLink
         className="text-decoration-none"
         to={pdf}
@@ -17,19 +30,32 @@ const About = () => {
         Resume
       </NavLink>
       <div className="d-flex row pt-4">
-        <div className="col-lg-2">
+        <h1>Skills</h1>
+        <div className="col-12 col-sm-3">
           <h4>{skills.Lang.Name}</h4>
           {skills.Lang.Skills.map((name, id) => (
             <li key={id}>{name}</li>
           ))}
         </div>
-        <div className="col-lg-2">
+        <div className="col-12 col-sm-6">
           <h4>{skills.Plat.Name}</h4>
           {skills.Plat.Skills.map((name, id) => (
             <li key={id}>{name}</li>
           ))}
         </div>
       </div>
+      {/* <div className="row pt-4">
+        <h1>Education</h1>
+        {education.Education.map((school, id) => (
+          <li key={id}>{school.school}</li>
+        ))}
+      </div> */}
+      {/* <div className="row pt-4">
+        <h1>Certificates</h1>
+        {education.Certifications.map((school, id) => (
+          <li key={id}>{school.school}</li>
+        ))}
+      </div> */}
     </div>
   );
 };
