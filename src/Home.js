bdc089b197ddc6c900c853d84f004ba264b172a8
Michael <39811434+msawlani@users.noreply.github.com>
2023-05-29 13:30:44 -0400

    updated home
    removed the img from a suggestion and centered the text. also still working on animation

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,11 @@ import "./CSS/home.css";
 const App = () => {
   return (
     <div className="container home">
-      <div className="row">
-        <div className="col-12 col-sm-5 offset-sm-1 align-self-center">
-          <span className="text-white fs-1 fw-bold">
-            Full Stack Developer determined on building products that customers
-            want.
-          </span>
-        </div>
-
-        <div className="col-12 col-sm-5">
-          <img
-            className="img-fluid opacity-50"
-            src={picture}
-            alt="Michael Sawlani's Face"
-          />
-        </div>
+      <div className="col-12 col-sm-5 align-items-center d-flex justify-content-center 100-vh">
+        <span className="text-white fs-1 fw-bold position-absolute centerText">
+          Full Stack Developer determined on building products that customers
+          want.
+        </span>
       </div>
     </div>
   );
